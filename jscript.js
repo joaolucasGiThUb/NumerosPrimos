d2a67fa534df203a8de60d8ba01b9c39aa4fedbe
João Lucas Benício de Araujo <joaolucasaraujo17@gmail.com>
2022-04-26 00:07:17 -0300

    update

---
+++ jscript.js
@@ -0,0 +1,18 @@
+//Criar uma função para mostrar os números primos
+
+exibirNumerosPrimos(11);
+
+function exibirNumerosPrimos(limite) {
+  for (let numero = 2; numero <= limite; numero++) {
+    if (NumeroPrimo(numero)) console.log(numero);
+  }
+}
+
+function NumeroPrimo(numero) {
+  for (let divisor = 2; divisor < numero; divisor++) {
+    if (numero % divisor === 0) {
+      return false;
+    }
+  }
+  return true;
+}
